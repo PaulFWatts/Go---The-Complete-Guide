1306c7230b954bf52f8382a5bb4c43d289024512
Paul Watts <paul.watts@me.com>
2023-12-08 19:12:54 +1100

    Add future real value calculation accounting for inflation

--- 16 Investment-Calculator/investment_calculator.go
+++ 16 Investment-Calculator/investment_calculator.go
@@ -6,12 +6,15 @@ import (
 )
 
 func main() {
+	const inflationRate = 2.5 
 	var investmentAmount, years float64 = 1000, 10 
 	expectedReturnRate := 5.5 // declared as float64 by default
 	
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 	fmt.Println("Press return to continue...")
 	fmt.Scanln()
 }
